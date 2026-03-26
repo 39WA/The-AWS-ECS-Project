64509701e84900d5bbab6655d72f3e3400b06fe1
39WA <wajid.ali786@hotmail.co.uk>
2026-03-26 12:00:32 +0000

    main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -140,23 +140,18 @@ resource "aws_lb_target_group" "tg" {
   }
 }
 
-# -----------------------------
-# Target Group
-# -----------------------------
-resource "aws_lb_target_group" "tg" {
-  name     = "threatmod-tg"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.main.id
-  target_type = "ip"
 
-  health_check {
-    path                = "/health"
-    interval            = 30
-    timeout             = 5
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-    matcher             = "200"
+
+# -----------------------------
+# Listener
+# -----------------------------
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
   }
 }
-
